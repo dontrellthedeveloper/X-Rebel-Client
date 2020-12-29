6d12408b830ab8ec8d11d0c4415d15eabef9b2d5
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-29 15:45:43 -0800

    added header component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import {Navbar, Footer} from "./components/Utils";
 import GlobalStyle from './globalStyles';
+
 import HomeSection from './pages/HomeSection';
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+
+import Header from "./components/nav/Header";
+
 import ScrollToTop from './components/Utils/ScrollToTop';
 
 
@@ -17,8 +23,11 @@ class App extends Component {
                     <GlobalStyle/>
                     <ScrollToTop/>
                     <Navbar/>
+                    <Header />
                     <Switch>
                         <Route exact path="/" component={HomeSection}/>
+                        <Route exact path="/login" component={Login}/>
+                        <Route exact path="/register" component={Register}/>
 
                     </Switch>
                     <Footer/>
