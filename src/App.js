731872646a907d31b9cc6183dac58ba7c228c5f6
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2020-12-31 10:27:09 -0800

    added conditional rendering to the header component. Added forgot password component and implementation. Redirecting user from the login/register/forgotpassword page if they are in redux state.

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ import RegisterComplete from "./pages/auth/RegisterComplete";
 
 import ScrollToTop from './components/Utils/ScrollToTop';
 
+import ForgotPassword from "./pages/auth/ForgotPassword";
+
 import { auth } from "./firebase";
 import { useDispatch } from "react-redux";
 
@@ -54,6 +56,7 @@ const App = () => {
                         <Route exact path="/login" component={Login}/>
                         <Route exact path="/register" component={Register}/>
                         <Route exact path="/register/complete" component={RegisterComplete} />
+                        <Route exact path="/forgot/password" component={ForgotPassword} />
                     </Switch>
                     <Footer/>
                 </Router>
