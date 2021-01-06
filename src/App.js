b29a712e91c35cb4fdedb1657718002fcf8e399b
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-01-05 23:08:11 -0800

    added view products page

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ import SubUpdate from "./pages/admin/sub/SubUpdate";
 import ProductCreate from "./pages/admin/product/ProductCreate";
 import AllProducts from "./pages/admin/product/AllProducts";
 import ProductUpdate from "./pages/admin/product/ProductUpdate";
+import Product from "./pages/Product";
 
 
 import { auth } from "./firebase";
@@ -91,6 +92,7 @@ const App = () => {
                         <AdminRoute exact path="/admin/product" component={ProductCreate} />
                         <AdminRoute exact path="/admin/products" component={AllProducts} />
                         <AdminRoute exact path="/admin/product/:slug" component={ProductUpdate}/>
+                        <Route exact path="/product/:slug" component={Product} />
                     </Switch>
                     <Footer/>
                 </Router>
